1ee51b053ed814293cda99de27b2002f446d662e
minhphamhk123 <minhphamhklovetory@gmail.com>
2024-03-21 18:18:08 +0700

    Update validator

--- lib/utils/validators/validation.dart
+++ lib/utils/validators/validation.dart
@@ -16,5 +16,34 @@ class TValidator {
     if (value == null || value.isEmpty) {
       return 'Password is required';
     }
+
+    if (value.length < 6) {
+      return 'Password must be at least 6 characters long. ';
+    }
+    // Check for uppercase letters
+    if (!value.contains(RegExp(r'[A-Z]'))) {
+      return 'Password must contain at least one uppercase letter.';
+    }
+    // Check for numbers
+    if (!value.contains(RegExp(r'[0-9]'))) {
+      return 'Password must contain at least one number.';
+    }
+    // Check for special characters
+    if (!value.contains(RegExp(r'[!@#$%^&*(),.?"{}|<>]'))) {
+      return 'Password must contain at least one special character.';
+    }
+    return null;
+  }
+
+  static String? validatePhoneNumber(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Phone number is required.';
+    }
+    // Regular expression for phone number validation a It-digit LIS phone no—ber format)
+    final phoneRegExp = RegExp(r'^\d{10}$');
+    if (!phoneRegExp.hasMatch(value)) {
+      return 'Invalid phone format (10 digits required).';
+    }
+    return null;
   }
 }
