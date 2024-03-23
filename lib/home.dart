51d584eb359f142e13d4513ba51ba5ad6a63114b
Quản Minh Trung <20520336@gm.uit.edu.vn>
2024-03-23 17:42:43 +0700

    create search-bar, horizontal-listview
    added:
    section-heading
    home-categories
    vertical-image-text
    search-container

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,7 @@
 import 'package:e_store/home_appbar.dart';
+import 'package:e_store/section_heading.dart';
 import 'package:e_store/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:e_store/widgets/custom_shapes/containers/search_container.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget{
@@ -15,7 +17,28 @@ class HomeScreen extends StatelessWidget{
             TPrimaryHeaderContainer(
               child: Column(
                 children: [
-                  THomeAppBar()
+                  ///Header
+
+                  THomeAppBar(),
+                  SizedBox(height: 30),
+                  ///Searchbar
+
+                  TSearchContainer(text: 'Search in store'),
+                  SizedBox(height: 30),
+
+                  ///Categories
+                  Padding(
+                    padding:  EdgeInsets.only(left: 30),
+                    child: Column(
+                      children:[
+                        ///Heading
+                        TSectionHeading(title: 'Popular Categories',showActionButton: false,textColor: Colors.white),
+                        SizedBox(height: 30);
+                        ///Categories
+                        THomeCategories(),
+                      ]
+                    )
+                  )
                 ],)
             ),
           ],
---
+++ lib/section_heading.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class TSectionHeading extends StatelessWidget{
+  const TSectionHeading({
+    super.key,
+    this.onPressed,
+    this.textColor,
+    this.buttonTitle = 'View All',
+    required this.title,
+    this.showActionButton = true,
+  });
+
+  final Color? textColor;
+  final bool showActionButton;
+  final String title, buttonTitle;
+  final void Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Row(
+      children:[
+        Text(title,/*style: ,*/ maxLines: 1,overflow: TextOverflow.ellipsis),
+        if (showActionButton) TextButton(onPressed: onPressed,child: Text(buttonTitle))
+      ],
+    );
+  }
+}
