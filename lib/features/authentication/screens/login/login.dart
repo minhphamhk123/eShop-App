465059241618c257d18684028e53e358361e81d4
qv2002 <20520866@gm.uit.edu.vn>
2024-03-23 18:13:57 +0700

    add more text_string for formLogin

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -1,36 +1,56 @@
-import 'dart:js';
-import 'dart:js_interop';
 
-import 'package:e_store/common/styles/spacing_styles.dart';
+
+import 'package:e_store/utils/constants/sizes.dart';
+import 'package:e_store/utils/constants/text_strings.dart';
 import 'package:flutter/material.dart';
+import 'package:e_store/common/styles/spacing_styles.dart';
+import 'package:e_store/utils/constants/image_strings.dart';
+import 'package:e_store/utils/helpers/helper_functions.dart';
+import 'package:iconsax/iconsax.dart';
+
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final dark = THelperFunction.IsDarkMode(context);
-    return const Scaffold(
+    final dark = THelperFunctions.isDarkMode(context);
+    return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
          padding: TSpacingStyle.paddingWithAppBarHeight,
-          child: Column(
-            children: [
-              /// Logo,Title & Sub-Title
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image(
-                    height: 150,
-                    image: AssetImage(dark ? TImages.lightAppLogo : TImages.darkAppLogo),
-              ),
-                  Text(TTexts.loginTitle, style: Theme.of(context).textTheme.headlineMedium),
-                  const SizedBox(height: TSizes.sm),
-                  Text(TTexts.loginSubTitle, style: Theme.of(context).textTheme.bodyMedium),
-            ],
-          ),
-        ),
-      )
-    );
+         child: Column(
+           children: [
+             ///Logo, Title & Sub-Title
+             Column(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: [
+                 Image(
+                   height: 150,
+                   image: AssetImage(dark ? TImages.lightAppLogo : TImages.darkAppLogo),
+                 ),
+                 Text(TTexts.loginTitle, style: Theme.of(context).textTheme.headlineMedium),
+                 const SizedBox(height: TSizes.sm),
+                 Text(TTexts.loginSubTitle, style: Theme.of(context).textTheme.bodyMedium),
+               ],
+             ),
+             /// Form
+             Form(child: Column(
+               children: [
+                 ///Email
+                 TextFormField(
+                   decoration: InputDecoration(
+                     prefixIcon: Icon(Iconsax.direct_right),
+                     labelText: TTexts.onBoardingTitle3,
+                   ),
+                 )
+                 ///Password
+               ],
+             ))
+           ],
+         ),
+       ),
+     ),
+   );
   }
 }
