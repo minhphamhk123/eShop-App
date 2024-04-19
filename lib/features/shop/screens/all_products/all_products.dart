a501097d9b8eb8246696fc3310c57f0855f41953
Quản Minh Trung <20520336@gm.uit.edu.vn>
2024-04-19 23:47:41 +0700

    added brand page
    + add sortable for products
    + add all products page
    + add all brands page
    + add brand products page

--- lib/features/shop/screens/all_products/all_products.dart
+++ lib/features/shop/screens/all_products/all_products.dart
@@ -6,6 +6,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 
+import '../../../../common/widgets/products/sortable/sortable_products.dart';
+
 class AllProducts extends StatelessWidget{
   const AllProducts({super.key});
 
@@ -13,31 +15,17 @@ class AllProducts extends StatelessWidget{
   Widget build(BuildContext context)
   {
 
-    return Scaffold(
+    return const Scaffold(
+      ///Appbar
       appBar: const TAppBar(title: Text('Popular Products'), showBackArrow: true),
       body: SingleChildScrollView(
         child: Padding(
             padding: const EdgeInsets.all(TSizes.defaultSpace),
-            child: Column(
-              children: [
-                ///Dropdown
-                DropdownButtonFormField(
-                    decoration: const InputDecoration(prefix: Icon(Iconsax.sort)),
-                    onChanged: (value){},
-                    items: ['Name','Higher Price','Lower Price','Sale','Newest','Popularity']
-                        .map((option) => DropdownMenuItem(
-                        value: option,
-                        child: Text(option)))
-                        .toList(),
-                ),
-                const SizedBox(height: TSizes.spaceBtwSections),
-                ///Products
-                TGridLayout(itemCount: 4, itemBuilder: (_, index) => const TProductCardVertical())
-              ],
-            )
+            child: TSortableProducts(),
         )
       ),
 
     );
   }
 }
+
