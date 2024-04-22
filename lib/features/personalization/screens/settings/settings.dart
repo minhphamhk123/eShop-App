05c0ea7a93fcfd4984350615c199c9be408b56c6
qv2002 <20520866@gm.uit.edu.vn>
2024-04-22 21:43:44 +0700

    fix merge missing in SettingScren

--- lib/features/personalization/screens/settings/settings.dart
+++ lib/features/personalization/screens/settings/settings.dart
@@ -4,7 +4,6 @@ import 'package:e_store/features/personalization/screens/address/address.dart';
 import 'package:e_store/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:iconsax/iconsax.dart';
 
 import '../../../../common/widgets/appbar/appbar.dart';
@@ -14,11 +13,11 @@ import '../../../../section_heading.dart';
 import '../../../../utils/constants/colors.dart';
 import '../profile/profile.dart';
 
-class SettingScreen extends StatelessWidget{
+class SettingScreen extends StatelessWidget {
   const SettingScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
         body: SingleChildScrollView(
             child: Column(
@@ -37,7 +36,8 @@ class SettingScreen extends StatelessWidget{
               const SizedBox(height: TSizes.spaceBtwSections),
 
               ///User Profile Card
-              TUserProfileTile(onPressed: () => Get.to(() => const ProfileScreen())),
+              TUserProfileTile(
+                  onPressed: () => Get.to(() => const ProfileScreen())),
               const SizedBox(height: TSizes.spaceBtwSections),
             ],
           ),
@@ -55,13 +55,37 @@ class SettingScreen extends StatelessWidget{
                 ),
                 const SizedBox(height: TSizes.spaceBtwItems),
 
-                  TSettingsMenuTile(icon: Iconsax.icon, title: 'My Address', subTitle: 'Set shopping delivery address'),
-                  TSettingsMenuTile(icon: Iconsax.shopping_cart, title: 'My Cart', subTitle: 'Add, remove products and move to checkout'),
-                  TSettingsMenuTile(icon: Iconsax.bag_tick, title: 'My Orders', subTitle: 'In-progress and Completed Orders', onTap: () => Get.to(() => const OrderScreen())),
-                  TSettingsMenuTile(icon: Iconsax.bank, title: 'Bank Account', subTitle: 'Withdraw balance to registered bank accounts'),
-                  TSettingsMenuTile(icon: Iconsax.discount_shape, title: 'My Coupons', subTitle: 'List of all the discounted coupons'),
-                  TSettingsMenuTile(icon: Iconsax.notification, title: 'Notifications', subTitle: 'Set any kind of notification messages'),
-                  TSettingsMenuTile(icon: Iconsax.security_card, title: 'Account Privacy', subTitle: 'Manage data usage and connect accounts'),
+                TSettingsMenuTile(
+                  icon: Iconsax.icon,
+                  title: 'My Address',
+                  subTitle: 'Set shopping delivery address',
+                  onTap: () => Get.to(() => const UserAddressScreen()),
+                ),
+                const TSettingsMenuTile(
+                    icon: Iconsax.shopping_cart,
+                    title: 'My Cart',
+                    subTitle: 'Add, remove products and move to checkout'),
+                TSettingsMenuTile(
+                    icon: Iconsax.bag_tick,
+                    title: 'My Orders',
+                    subTitle: 'In-progress and Completed Orders',
+                    onTap: () => Get.to(() => const OrderScreen())),
+                const TSettingsMenuTile(
+                    icon: Iconsax.bank,
+                    title: 'Bank Account',
+                    subTitle: 'Withdraw balance to registered bank accounts'),
+                const TSettingsMenuTile(
+                    icon: Iconsax.discount_shape,
+                    title: 'My Coupons',
+                    subTitle: 'List of all the discounted coupons'),
+                const TSettingsMenuTile(
+                    icon: Iconsax.notification,
+                    title: 'Notifications',
+                    subTitle: 'Set any kind of notification messages'),
+                const TSettingsMenuTile(
+                    icon: Iconsax.security_card,
+                    title: 'Account Privacy',
+                    subTitle: 'Manage data usage and connect accounts'),
 
                 ///App Settings
                 const SizedBox(height: TSizes.spaceBtwSections),
@@ -92,13 +116,13 @@ class SettingScreen extends StatelessWidget{
                 ),
 
                 ///Logout Button
-                SizedBox(height: TSizes.spaceBtwSections),
+                const SizedBox(height: TSizes.spaceBtwSections),
                 SizedBox(
                   width: double.infinity,
                   child:
-                      OutlinedButton(onPressed: () {}, child: Text('Logout')),
+                      OutlinedButton(onPressed: () {}, child: const Text('Logout')),
                 ),
-                SizedBox(height: TSizes.spaceBtwSections * 2.5),
+                const SizedBox(height: TSizes.spaceBtwSections * 2.5),
               ],
             ))
       ],
